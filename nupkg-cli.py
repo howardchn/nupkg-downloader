0465899ace33c87cc5e184f996a8b224394af97e
howardch@outlook.com <howardch@outlook.com>
2016-07-29 11:17:06 +0800

    Added help and support download list.

--- nupkg-cli.py
+++ nupkg-cli.py
@@ -1,6 +1,8 @@
 from urllib.request import urlopen
 from html.parser import HTMLParser
 from os import path
+from os import getcwd
+import sys
 
 class NupkgHTMLParser (HTMLParser):
     def __init__ (self):
@@ -27,26 +29,27 @@ class NupkgHTMLParser (HTMLParser):
         if self.dependenciesInTracking and tag == 'ul':
             self.dependenciesInTracking = False
 
-def downloadNupkg(url, targetdir): 
-    print ('Parsing Dependency:', url)
-    response = urlopen(url)
-    content = str(response.read())
+downloaded_urls = []
+def download_nupkg(url, targetdir): 
+    if not url in downloaded_urls:  
+        downloaded_urls.append (url)
+        print ('Parsing Dependency:', url)
+        response = urlopen(url)
+        content = str(response.read())
 
-    parser = NupkgHTMLParser();
-    parser.feed(content);
-    parser.close()
+        parser = NupkgHTMLParser();
+        parser.feed(content);
+        parser.close()
 
-    if (parser.pkgUrl != ''):
-        downloadUriTo(parser.pkgUrl, targetdir)
+        if (parser.pkgUrl != ''):
+            download_url(parser.pkgUrl, targetdir)
 
-    host = getHost(url)
-    for dependency in parser.dependencies:
-        dependencyUrl = host + dependency
-        downloadNupkg(dependencyUrl, targetdir)
+        host = getHost(url)
+        for dependency in parser.dependencies:
+            dependencyUrl = host + dependency
+            download_nupkg(dependencyUrl, targetdir)
 
-    print ('Task complete.')
-
-def downloadUriTo(url, dirname):
+def download_url(url, dirname):
     print ('Downloading package:', url)
     pkgResponse = urlopen(url)
     pkgUrl = str(pkgResponse.url)
@@ -66,4 +69,46 @@ def getHost(url):
     host = ''.join([schema, domain])
     return host
 
-downloadNupkg('https://www.nuget.org/packages/NetTopologySuite/', r'e:\\tmp')
+def strip_arg(arg):
+    arg = arg.strip()
+    arg = arg.strip('"')
+    arg = arg.strip("'")
+    return arg
+
+def help (): 
+    print ('''This is NuGet package downloader. It downloads the package and its dependencies recursively.
+    \tusage: nupkg-cli.py nupkg_url [out_dir] [-h | --h]''')
+
+def get_download_list (list_path):
+    with open (list_path, 'r') as file:
+        lines = file.readlines ()
+        return lines
+
+def main (argv):
+    url = '' # 'https://www.nuget.org/packages/NetTopologySuite' 
+    output_dir = getcwd()
+    
+    if len (argv) == 1 or '-h' in argv or '--h' in argv:
+        help()
+    else: 
+        if len (argv) >= 2:
+            url = strip_arg (argv[1]) 
+        if len (argv) >= 3:
+            output_dir = strip_arg (argv[2])
+        if not bool (url):
+            help()
+        else:
+            ext = path.splitext (url)[1]
+            if ext == '.txt':
+                urls = get_download_list(url)
+                for tmp_url in urls:
+                    download_nupkg(tmp_url, output_dir)
+            else:
+                download_nupkg(url, output_dir)
+            print ('Task complete.')
+    
+main (sys.argv)
+
+# python nupkg-cli.py E:\VS-Git\Python\nupkg-downloader\tasks.txt E:\VS-Git\ThinkGeo-NuPkgs
+
+
