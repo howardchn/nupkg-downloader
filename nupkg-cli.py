932d4f375d987e102b32a06de76c906c4b6fdfc6
howardch@outlook.com <howardch@outlook.com>
2016-07-29 14:31:03 +0800

    added more task

--- nupkg-cli.py
+++ nupkg-cli.py
@@ -4,14 +4,16 @@ from os import path
 from os import getcwd
 import sys
 
+
 class NupkgHTMLParser (HTMLParser):
-    def __init__ (self):
+
+    def __init__(self):
         super(NupkgHTMLParser, self).__init__()
         self.pkgUrl = ''
         self.dependencies = []
         self.dependenciesInTracking = False
 
-    def handle_starttag (self, tag, attrs):
+    def handle_starttag(self, tag, attrs):
         if tag == 'a':
             attrsDict = dict(attrs)
             if self.dependenciesInTracking and 'href' in attrsDict:
@@ -25,20 +27,22 @@ class NupkgHTMLParser (HTMLParser):
             if 'class' in attrsDict and attrsDict['class'] == 'dependencySet':
                 self.dependenciesInTracking = True
 
-    def handle_endtag (self, tag):
+    def handle_endtag(self, tag):
         if self.dependenciesInTracking and tag == 'ul':
             self.dependenciesInTracking = False
 
 downloaded_urls = []
-def download_nupkg(url, targetdir): 
-    if not url in downloaded_urls:  
-        downloaded_urls.append (url)
-        print ('Parsing Dependency:', url)
+
+
+def download_nupkg(url, targetdir):
+    if not url in downloaded_urls:
+        downloaded_urls.append(url)
+        print('Parsing Dependency:', url)
         response = urlopen(url)
         content = str(response.read())
 
-        parser = NupkgHTMLParser();
-        parser.feed(content);
+        parser = NupkgHTMLParser()
+        parser.feed(content)
         parser.close()
 
         if (parser.pkgUrl != ''):
@@ -49,14 +53,16 @@ def download_nupkg(url, targetdir):
             dependencyUrl = host + dependency
             download_nupkg(dependencyUrl, targetdir)
 
+
 def download_url(url, dirname):
-    print ('Downloading package:', url)
+    print('Downloading package:', url)
     pkgResponse = urlopen(url)
     pkgUrl = str(pkgResponse.url)
     pkgName = path.basename(pkgUrl)
     pkgFullname = path.join(dirname, pkgName)
-    with open (pkgFullname, 'wb') as output:
+    with open(pkgFullname, 'wb') as output:
         output.write(pkgResponse.read())
+
 
 def getHost(url):
     schema = ''
@@ -69,46 +75,49 @@ def getHost(url):
     host = ''.join([schema, domain])
     return host
 
+
 def strip_arg(arg):
     arg = arg.strip()
     arg = arg.strip('"')
     arg = arg.strip("'")
     return arg
 
-def help (): 
+
+def help():
     print ('''This is NuGet package downloader. It downloads the package and its dependencies recursively.
     \tusage: nupkg-cli.py nupkg_url [out_dir] [-h | --h]''')
 
-def get_download_list (list_path):
-    with open (list_path, 'r') as file:
-        lines = file.readlines ()
+
+def get_download_list(list_path):
+    with open(list_path, 'r') as file:
+        lines = file.readlines()
         return lines
 
-def main (argv):
-    url = '' # 'https://www.nuget.org/packages/NetTopologySuite' 
+
+def main(argv):
+    url = ''  # 'https://www.nuget.org/packages/NetTopologySuite'
     output_dir = getcwd()
-    
-    if len (argv) == 1 or '-h' in argv or '--h' in argv:
+
+    if len(argv) == 1 or '-h' in argv or '--h' in argv:
         help()
-    else: 
-        if len (argv) >= 2:
-            url = strip_arg (argv[1]) 
-        if len (argv) >= 3:
-            output_dir = strip_arg (argv[2])
-        if not bool (url):
+    else:
+        if len(argv) >= 2:
+            url = strip_arg(argv[1])
+        if len(argv) >= 3:
+            output_dir = strip_arg(argv[2])
+        if not bool(url):
             help()
         else:
-            ext = path.splitext (url)[1]
+            ext = path.splitext(url)[1]
             if ext == '.txt':
                 urls = get_download_list(url)
                 for tmp_url in urls:
                     download_nupkg(tmp_url, output_dir)
             else:
                 download_nupkg(url, output_dir)
-            print ('Task complete.')
-    
-main (sys.argv)
+            print('Task complete.')
 
-# python nupkg-cli.py E:\VS-Git\Python\nupkg-downloader\tasks.txt E:\VS-Git\ThinkGeo-NuPkgs
+main(sys.argv)
 
-
+# python nupkg-cli.py E:\VS-Git\Python\nupkg-downloader\tasks.txt
+# E:\VS-Git\ThinkGeo-NuPkgs
